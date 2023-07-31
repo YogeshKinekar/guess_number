c2902fdf746b4674ba8d951b02af15a8a10facc0
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-07-31 23:30:58 +0530

    feat: Added Card look shadow

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,20 @@
  import React from "react";
  import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
+ import Card from "../components/Card";
+
  const StartGameScreen = props => {
     return (
  <View style={styles.screen}>
  <Text style={styles.title}>Start a New Game!</Text>
- <View style={styles.inputContainer}>
+ <Card style={styles.inputContainer}>
    <Text>Select a Number</Text>
    <TextInput />
    <View style={styles.buttonContainer}>
       <Button title="RESET" onPress={() => {}}/>
       <Button title="CONFIRM" onPress={() => {}}/>
    </View>
- </View>
+ </Card>
  </View>
     );
  }
