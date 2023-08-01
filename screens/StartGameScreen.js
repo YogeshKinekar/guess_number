34bb9d6ecaa24a261145a1f21c5d28ff461756d6
yogeshkinekar <121310142+YogeshKinekar@users.noreply.github.com>
2023-08-01 17:45:07 +0530

    feat: Added configuring textinput

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,53 +1,64 @@
- import React from "react";
- import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
 
- import Card from "../components/Card";
- import colors from "./constants/colors";
+import Card from "../components/Card";
+import Input from "../components/input";
+import colors from "./constants/colors";
 
- const StartGameScreen = props => {
-    return (
- <View style={styles.screen}>
- <Text style={styles.title}>Start a New Game!</Text>
- <Card style={styles.inputContainer}>
-   <Text>Select a Number</Text>
-   <TextInput />
-   <View style={styles.buttonContainer}>
-    <View style={styles.button}>
-      <Button title="RESET" onPress={() => {}} color={colors.accent}/>
-      </View>
-      <View style={styles.button}>
-      <Button title="CONFIRM" onPress={() => {}} color={colors.primary}/>
-      </View>
-   </View>
- </Card>
- </View>
-    );
- }
+const StartGameScreen = props => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <Card style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <Input
+          style={styles.input}
+          blurOnSubmit
+          autoCapitalize='none'
+          autoCorrect={false}
+          keyboardType="numeric"
+          maxLength={2} />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="RESET" onPress={() => { }} color={colors.accent} />
+          </View>
+          <View style={styles.button}>
+            <Button title="CONFIRM" onPress={() => { }} color={colors.primary} />
+          </View>
+        </View>
+      </Card>
+    </View>
+  );
+}
 
- const styles = StyleSheet.create({
-    screen :{
-        flex: 1,
-        padding: 10,
-        alignItems: 'center'
-    },
-    title: {
-      fontSize:20,
-      marginVertical: 10
-    },
-    inputContainer: {
-      width : 300,
-      maxWidth : '80%',
-      alignItems: 'center'
-    },
-    buttonContainer: {
-      flexDirection: 'row',
-      width: '100%',
-      justifyContent: 'space-between',   
-    },
-    button: {
-      width: 100
-    }
- });
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+  },
+  button: {
+    width: 100
+  },
+  input: {
+    width: 50,
+    textAlign: 'center'
+  }
+});
 
 
- export default StartGameScreen;
+export default StartGameScreen;
